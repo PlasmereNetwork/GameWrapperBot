829cd421c13e3e9321c8f83a4d062ffc865bf290
VTCAKAVSMoACE <vtcakavsmoace@gmail.com>
2018-07-02 18:52:48 -0500

    Oh gitlab pages works differently

--- docs/javadoc/member-search-index.js
+++
@@ -1,6 +0,0 @@
-memberSearchIndex = [{
-    "p": "info.addisoncrump.oklahoma.bot",
-    "c": "OklahomaBotApplication",
-    "l": "main(String[])",
-    "url": "main(java.lang.String[])"
-}, {"p": "info.addisoncrump.oklahoma.bot", "c": "OklahomaBotApplication", "l": "OklahomaBotApplication()"}]
--- docs/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p": "info.addisoncrump.oklahoma.bot", "l": "OklahomaBotApplication"}]
